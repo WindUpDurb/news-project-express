7a99fa39ae8b2e804c85dc19c1febd4b8347b897
David Urbina <durbina1991@gmail.com>
2016-08-27 12:45:53 -0700

    Front-End Progress/News Tiles

--- src/components/common/NewsTile.js
+++ src/components/common/NewsTile.js
@@ -5,16 +5,12 @@ import React, {PropTypes} from "react";
 export const NewsTile = ({newsObject}) => {
     console.log(newsObject);
     return (
-        <div className="col-md-4 cardEffect newsTile">
-            <div className="row">
-                <div className="col-md-7">
-                    <img src={newsObject.image} className="newsTileImage"/>
-                </div>
-                <div className="col-md-5">
-                    <div className="text-center">
-                        <a className="newsTileTitle" href={newsObject.link}>{newsObject.title}</a>
-                    </div>
-                </div>
+        <div style={{backgroundImage: `url(${newsObject.image})`}} className="col-md-3 newsTile">
+            <div className="newsTileInfo text-center">
+                <span className="tileTitle">{newsObject.title}</span>
+            </div>
+            <div className="tileIconDiv">
+                <img src="/statics/cnnIcon.png" className="newsIcon"/>
             </div>
         </div>
 
